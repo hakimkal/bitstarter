e462a829f8b6f0bee266e595c514722f2e4a1fb2
Ubuntu <ubuntu@ip-10-164-56-209.ec2.internal>
2013-07-15 09:20:09 +0000

    updated web.js to read from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+resp = fs.readFileSync("index.html");
+buf = new Buffer(resp) ;
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
